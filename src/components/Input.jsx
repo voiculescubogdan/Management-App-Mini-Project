f35364b9b3c5068d4e908699d1ce8148e725b6bf
voiculescubogdan <voiculescubogdan22@stud.ase.ro>
2025-06-27 23:30:04 +0300

    initial commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,16 @@
+export default function Input({ label, textarea, ref, ...props}) {
+  const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+
+  return (
+    <p className="flex flex-col gap-1 my-4">
+      <label htmlFor="" className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </label>
+      {textarea ? (
+        <textarea ref={ref} {...props} className={classes} />
+      ) : (
+        <input ref={ref} {...props} className={classes} />
+      )}
+    </p>
+  );
+}
